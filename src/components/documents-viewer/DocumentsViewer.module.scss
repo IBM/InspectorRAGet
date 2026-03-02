ae44c7d482768d6997b29d643072d6ecd6e75bb2
Kshitij Fadnis <cool.kshij@gmail.com>
2026-03-02 15:17:39 -0500

    chore (lint): Update copyright to 2023-present, add license header enforcement.
    Signed-off-by: Kshitij Fadnis <cool.kshij@gmail.com>

--- src/components/documents-viewer/DocumentsViewer.module.scss
+++ src/components/documents-viewer/DocumentsViewer.module.scss
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2023-2025 InspectorRAGet Team
+ * Copyright 2023-present InspectorRAGet Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
